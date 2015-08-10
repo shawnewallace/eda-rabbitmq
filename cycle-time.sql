48f4f00e24f6ac0ffd8d92d3eefe1d77d66dc075
Shawn Wallace <shawnewallace@gmail.com>
2015-08-10 11:53:07 -0400

    Added db script for looking at cycle time of the process.

---
+++ cycle-time.sql
@@ -0,0 +1,8 @@
+﻿select OrderId,
+	min(WhenReceived) as small,
+	max(whenReceived) as large,
+	datediff(s, min(WhenReceived), max(WhenReceived)) as cycletime,
+	count(*)
+from app.log_entries
+group by OrderId
+order by cycletime desc;
